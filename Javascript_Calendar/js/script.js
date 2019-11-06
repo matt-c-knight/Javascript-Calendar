579947b912771471efafb4bbe0a1961f69e2a59d
Matthew Knight <mattknight.sa@gmail.com>
2019-11-06 12:55:14 -0600

    preliminary results

--- Javascript_Calendar/js/script.js
+++ Javascript_Calendar/js/script.js
@@ -52,6 +52,9 @@ $(".five").attr('data-time', 17)
 
 console.log(typeof currentTime)
 
+
+
+
 const nine = document.querySelector(".nine");
 const ten = document.querySelector(".ten");
 const eleven = document.querySelector(".eleven");
@@ -61,36 +64,76 @@ const two = document.querySelector(".two");
 const three = document.querySelector(".three");
 const four = document.querySelector(".four");
 const five = document.querySelector(".five");
-console.log(nine.dataset.time)
 
-
-
-if (nine.dataset.time < currentTime) {
-    $(".9").css("background-color", "grey");
+if (nine.dataset.time > currentTime) {
+    $(".9").css("background-color", "green");
 }else if(nine.dataset.time == currentTime) {
     $(".9").css("background-color", "red");
-} else {
-    $(".9").css("background-color", "green");
+} else if (ten.dataset.time < currentTime) {
+    $(".9").css("background-color", "gray");
 }
 
-if (ten.dataset.time < currentTime) {
-    $(".10").css("background-color", "grey");
+if (ten.dataset.time > currentTime) {
+    $(".10").css("background-color", "green");
 }else if(nine.dataset.time == currentTime) {
     $(".10").css("background-color", "red");
-} else {
-    $(".10").css("background-color", "green");
+} else if (ten.dataset.time < currentTime) {
+    $(".10").css("background-color", "gray");
+}
+if (eleven.dataset.time > currentTime) {
+    $(".11").css("background-color", "green");
+}else if(eleven.dataset.time == currentTime) {
+    $(".11").css("background-color", "red");
+} else if (eleven.dataset.time < currentTime) {
+    $(".11").css("background-color", "gray");
 }
 
-if (two.dataset.time < currentTime) {
-    $(".2").css("background-color", "grey");
-}else if(nine.dataset.time == currentTime) {
-    $(".2").css("background-color", "red");
-} else {
+if (twelve.dataset.time > currentTime) {
+    $(".12").css("background-color", "green");
+}else if(twelve.dataset.time == currentTime) {
+    $(".12").css("background-color", "red");
+} else if (twelve.dataset.time < currentTime) {
+    $(".12").css("background-color", "gray");
+}
+
+if (one.dataset.time > currentTime) {
+    $(".1").css("background-color", "green");
+}else if(one.dataset.time == currentTime) {
+    $(".1").css("background-color", "red");
+} else if (one.dataset.time < currentTime) {
+    $(".1").css("background-color", "gray");
+}
+
+if (two.dataset.time > currentTime) {
     $(".2").css("background-color", "green");
+}else if(two.dataset.time == currentTime) {
+    $(".2").css("background-color", "red");
+} else if (two.dataset.time < currentTime) {
+    $(".2").css("background-color", "gray");
+}
+if (three.dataset.time > currentTime) {
+    $(".3").css("background-color", "green");
+}else if(three.dataset.time == currentTime) {
+    $(".3").css("background-color", "red");
+} else if (three.dataset.time < currentTime) {
+    $(".3").css("background-color", "gray");
 }
 
+if (four.dataset.time > currentTime) {
+    $(".4").css("background-color", "green");
+}else if(four.dataset.time == currentTime) {
+    $(".4").css("background-color", "red");
+} else if (four.dataset.time < currentTime) {
+    $(".4").css("background-color", "gray");
+}
 
-
+if (five.dataset.time > currentTime) {
+    $(".5").css("background-color", "green");
+}else if(five.dataset.time == currentTime) {
+    $(".5").css("background-color", "red");
+} else if (five.dataset.time < currentTime) {
+    $(".5").css("background-color", "gray");
+}
 
 
 
