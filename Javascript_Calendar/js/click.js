7021d525f53cb5f0a6e76916b4e6036922cfe9db
Matthew Knight <mattknight.sa@gmail.com>
2019-11-08 19:18:27 -0600

    update with new file

---
+++ Javascript_Calendar/js/click.js
@@ -0,0 +1,128 @@
+
+$(".nine").attr('data-time', 9);
+$(".ten").attr('data-time', 10);
+$(".eleven").attr('data-time', 11);
+$(".twelve").attr('data-time', 12);
+$(".one").attr('data-time', 13);
+$(".two").attr('data-time', 14);
+$(".three").attr('data-time', 15);
+$(".four").attr('data-time', 16);
+$(".five").attr('data-time', 17);
+nineVal = $(".nine").data("time");
+tenVal = $(".ten").data("time");
+elevenVal = $(".eleven").data("time");
+twelveVal = $(".twelve").data("time");
+oneVal = $(".one").data("time");
+twoVal = $(".two").data("time");
+threeVall = $(".three").data("time");
+fourVal = $(".four").data("time");
+fiveVal = $(".five").data("time");
+var scheduleArray = [];
+
+$('.save9').click(function(event) {
+    event.preventDefault()
+    schedule = $('.9').val();
+ times = {
+        time: nineVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save10').click(function(event) {
+    event.preventDefault()
+    schedule = $('.10').val();
+    console.log(schedule);
+    times = {
+        time: tenVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save11').click(function(event) {
+    event.preventDefault()
+    schedule = $('.11').val();
+    console.log(schedule);
+    times = {
+        time: elevenVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save12').click(function(event) {
+    event.preventDefault()
+    schedule = $('.12').val();
+    console.log(schedule);
+    times = {
+        time: twelveval,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save1').click(function(event) {
+    event.preventDefault()
+    schedule = $('.1').val();
+    console.log(schedule);
+    times = {
+        time: oneVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save2').click(function(event) {
+    event.preventDefault()
+    schedule = $('.2').val();
+    console.log(schedule);
+    times = {
+        time: twoVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save3').click(function(event) {
+    event.preventDefault()
+    schedule = $('.3').val();
+    console.log(schedule);
+    times = {
+        time: threeVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save4').click(function(event) {
+    event.preventDefault()
+    schedule = $('.4').val();
+    console.log(schedule);
+    times = {
+        time: fourVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
+$('.save5').click(function(event) {
+    event.preventDefault()
+    schedule = $('.5').val();
+    console.log(schedule);
+    times = {
+        time: fiveVal,
+        input: schedule
+    }
+    scheduleArray.push(times);
+    scheduleArray = scheduleArray.concat(JSON.parse(localStorage.getItem('schedule') || '[]'));
+    window.localStorage.setItem('schedule', JSON.stringify(scheduleArray));
+    })
--- Javascript_Calendar/js/script.js
+++ Javascript_Calendar/js/script.js
@@ -1,59 +1,13 @@
-var schedule;
-
-// $('.save').click(function() {
-//     console.log(schedule);
-
-// })
-
-// $('.event').click(function() {
-//     schedule = $(this).val();
-//     return schedule
-// })
-
-$('.save').click(function(event) {
-    event.preventDefault()
-    schedule = $(this).val();
-    $('.schedule').attr('value', schedule);
-    console.log(schedule)
-    
-
-})
-
-
-
-let date = moment();
-// console.log(date)
-let currentTime = date.format("HH")
-currentTime = parseInt(currentTime)
-console.log(currentTime)
-
-
-
-
-
-
-$("h3").text(date.format("dddd, MMMM DD"));
-
-
-// var currentTime = moment("2013-03-24")
-// var date = moment();
-
-
-
-$(".nine").attr('data-time', 9)
-$(".ten").attr('data-time', 10)
-$(".eleven").attr('data-time', 11)
-$(".twelve").attr('data-time', 12)
-$(".one").attr('data-time', 13)
-$(".two").attr('data-time', 14)
-$(".three").attr('data-time', 15)
-$(".four").attr('data-time', 16)
-$(".five").attr('data-time', 17)
-
-console.log(typeof currentTime)
-
-
-
+$(document).ready(function(){
+$(".nine").attr('data-time', 9);
+$(".ten").attr('data-time', 10);
+$(".eleven").attr('data-time', 11);
+$(".twelve").attr('data-time', 12);
+$(".one").attr('data-time', 13);
+$(".two").attr('data-time', 14);
+$(".three").attr('data-time', 15);
+$(".four").attr('data-time', 16);
+$(".five").attr('data-time', 17);
 
 const nine = document.querySelector(".nine");
 const ten = document.querySelector(".ten");
@@ -64,6 +18,18 @@ const two = document.querySelector(".two");
 const three = document.querySelector(".three");
 const four = document.querySelector(".four");
 const five = document.querySelector(".five");
+var schedule;
+var user;
+
+
+    
+var date = moment();
+var currentTime = date.format("HH")
+currentTime = parseInt(currentTime)
+
+$("h3").text(date.format("dddd, MMMM DD"));
+
+
 
 if (nine.dataset.time > currentTime) {
     $(".9").css("background-color", "green");
@@ -136,5 +102,5 @@ if (five.dataset.time > currentTime) {
 }
 
 
-
+})
 
